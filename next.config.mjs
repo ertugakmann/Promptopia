411e3a51a8cd53220a1f05bac68335d1f7af4c56
ertugakmann <134059022+ertugakmann@users.noreply.github.com>
2024-07-20 20:57:04 +0300

    Updated .gitignore and next.config.mjs, add environment variables

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  experimental: {
+    appDir: true,
+    serverComponentsExternalPackages: ["mongoose"],
+  },
+  images: {
+    domains: ["lh3.googleusercontent.com"],
+  },
+  webpack(config) {
+    // Webpack ayarlarını güncelle
+    if (!config.experiments) {
+      config.experiments = {};
+    }
+    config.experiments.topLevelAwait = true;
+
+    return config;
+  },
 };
 
 export default nextConfig;
